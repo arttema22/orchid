7f1633917d59eb8b98c68f30274a3f492ea6c097
Artem Gusev <arttema@mail.ru>
2022-12-22 00:47:58 +0300

    New alert. Blocked email.

--- resources/views/order/new.blade.php
+++ resources/views/order/new.blade.php
@@ -5,48 +5,55 @@
 @section('description')Здесь можно оформить запрос на дополнительную услугу.@endsection
 
 @section('content')
-<form method="post" action="">
-    @csrf
-    <div class="row">
-        <div class="col-md-4">
-            @include('inc.fields.lastName')
-        </div>
-        <div class="col-md-4">
-            @include('inc.fields.firstName')
-        </div>
-        <div class="col-md-4">
-            @include('inc.fields.patronymic')
-        </div>
+<div class="row">
+    <div class="col-md-8">
+        <form method="post" action="">
+            @csrf
+            <div class="row">
+                <div class="col-md-4">
+                    @include('inc.fields.lastName')
+                </div>
+                <div class="col-md-4">
+                    @include('inc.fields.firstName')
+                </div>
+                <div class="col-md-4">
+                    @include('inc.fields.patronymic')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    @include('inc.fields.address')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    @include('inc.fields.phone')
+                </div>
+                <div class="col-md-6">
+                    @include('inc.fields.email')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    @include('inc.fields.services')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    @include('inc.fields.message')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    @include('inc.fields.personaldata')
+                    @include('inc.fields.agreerules')
+                </div>
+            </div>
+            @include('inc.fields.btn-submit')
+        </form>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            @include('inc.fields.address')
-        </div>
+    <div class="col-md-4">
+        @include('inc.alerts.blockedemail')
     </div>
-    <div class="row">
-        <div class="col-md-6">
-            @include('inc.fields.phone')
-        </div>
-        <div class="col-md-6">
-            @include('inc.fields.email')
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            @include('inc.fields.services')
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            @include('inc.fields.message')
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            @include('inc.fields.personaldata')
-            @include('inc.fields.agreerules')
-        </div>
-    </div>
-    @include('inc.fields.btn-submit')
-</form>
+</div>
 @endsection
