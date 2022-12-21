7fae46062df91269a219ad7d3023429ff62d084f
Artem Gusev <arttema@mail.ru>
2022-12-22 01:30:48 +0300

    Edit send letter

--- resources/views/emails/ticket/AnswerTicket.blade.php
+++ resources/views/emails/ticket/AnswerTicket.blade.php
@@ -1,9 +1,18 @@
-Уважаемый(ая) {{$mess->user}}.<br>
-От Вас поступило сообщение <b>"{{$mess->ticket_title}}"</b><br>
-<small>{{$mess->message}}</small><br>
-
-<h3>Вот наш ответ:</h3>
-{{$mess->answer}}
-
+Внимание! Это письмо будет приходить автору обращения. Прощу проверить текст и работу ссылок.
+<hr>
+Уважаемый(ая), {{$ticket->last_name}} {{$ticket->first_name}} {{$ticket->patronymic}}!<br>
+ООО «РКС-энерго» подготовлен ответ на Ваше обращение.<br>
 <br>
-<a href="{{$mess->ticket_url}}">Посмотреть переписку</a>
+<br>
+Персональный код, присвоенный Вашему обращению:
+<h3>{{$ticket->code}}</h3>
+<a href="{{route('reception.status')}}" target="_blank">Посмотреть обращение</a>
+<br>
+<br>
+Команда ООО «РКС-энерго».<br>
+Мы в социальных сетях:<br>
+ВКонтакте: <a href="https://vk.com/rks_energo" target="_blank">https://vk.com/rks_energo</a>
+telegram: <a href="https://t.me/s/rksenergo" target="_blank">https://t.me/s/rksenergo</a>
+<br>
+<br>
+Это автоматическое сообщение, пожалуйста, не отвечайте на него.
--- resources/views/emails/ticket/NewTicket.blade.php
+++ resources/views/emails/ticket/NewTicket.blade.php
@@ -1,13 +1,23 @@
 Внимание! Это письмо будет приходить автору обращения. Прощу проверить текст и работу ссылок.
 <hr>
-Здравствуйте, {{$ticket->last_name}} {{$ticket->first_name}} {{$ticket->patronymic}}.<br>
-На сайте РКС-энерго Вами оставлено следующее обращение:<br>
-<h4>{{$ticket->title}}</h4>
-{{$ticket->message}}
+Уважаемый(ая), {{$ticket->last_name}} {{$ticket->first_name}} {{$ticket->patronymic}}!<br>
+Благодарим Вас за обращение в ООО «РКС-энерго»!<br>
+Это автоматическое сообщение, подтверждающее, что мы получили запрос через наш сервис "Электронная приемная".<br>
 <br>
-Вашему обращению присвоен персональный код доступа:
+<br>
+Персональный код, присвоенный Вашему обращению:
 <h3>{{$ticket->code}}</h3>
 <a href="{{route('reception.status')}}" target="_blank">Посмотреть обращение</a>
 <br>
-В ближайшее время мы дадим ответ или свяжемся с Вами.<br>
-С уважением, команда РКС.
+Мы обязательно ответим Вам в течение трех рабочих дней!<br>
+<br>
+Также напоминаем, что ответы на наиболее частые вопросы Вы можете найти по ссылке:
+<a href="https://www.rks-energo.ru/fizicheskim-liczam" target="_blank">
+    https://www.rks-energo.ru/fizicheskim-liczam</a>
+<br>
+<br>
+<br>
+Команда ООО «РКС-энерго».<br>
+Мы в социальных сетях:<br>
+ВКонтакте: <a href="https://vk.com/rks_energo" target="_blank">https://vk.com/rks_energo</a>
+telegram: <a href="https://t.me/s/rksenergo" target="_blank">https://t.me/s/rksenergo</a>
